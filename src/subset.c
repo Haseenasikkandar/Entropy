733c77884f84b55714bbe0b7e4e05965f3c9db09
Haseenasikkandar <haseenasikkandartce@gmail.com>
2017-03-10 10:41:27 +0530

    Update subset.c

--- src/subset.c
+++ src/subset.c
@@ -88,7 +88,7 @@ void EvalSubset(Attribute Att, CaseCount Cases)
     Boolean	Better;	
 double count[20];
 int i=0;
-double alpha =3.25;
+double alpha =2.25;
 double q= 1/(1-alpha);
     /*  First compute Freq[][], ValFreq[], base info, and the gain
 	and total info of a split on discrete attribute Att  */
@@ -375,7 +375,7 @@ void Merge(DiscrValue x, DiscrValue y, CaseCount Cases)
     double	Entr=0;
     CaseCount	KnownCases=0;
     int		R, C;
-double alpha =3.25;
+double alpha =2.25;
 	int i=0;
 	double q=1/(1-alpha);
 	double count[20];
@@ -386,16 +386,16 @@ double alpha =3.25;
 	// GEnv.Freq[x][c]=   GEnv.Freq[x][c]/KnownCases;
 	Entr += pow(GEnv.Freq[x][c] ,alpha);
 	KnownCases += GEnv.Freq[x][c];	 
-	count[i] += GEnv.Freq[x][c]-GEnv.Freq[y][c];
+	//count[i] += GEnv.Freq[x][c]-GEnv.Freq[y][c];
     }
-	if(count[i]<0)
+	/*if(count[i]<0)
 	{
 		count[i] = -1 * count[i];
-	}
+	}*/
 	Entr /= KnownCases;
         Entr = (Entr -1)*q;
-	count[i] /= KnownCases;
-	Entr *= count[i];
+	//count[i] /= KnownCases;
+	//Entr *= count[i];
 	i++;
 	
 	GEnv.SubsetInfo[x] /= Cases;
@@ -453,7 +453,7 @@ void EvaluatePair(DiscrValue x, DiscrValue y, CaseCount Cases)
     ClassNo	c;
     double	Entr=0;
     CaseCount	KnownCases=0, F;
-double alpha=3.25;
+double alpha=2.25;
 	int i=0;
 	double count[20];
 double q = 1/(1-alpha);
@@ -475,17 +475,17 @@ double q = 1/(1-alpha);
 	//Entr -= F * Log(F);
 	Entr += pow(F,alpha);
 	KnownCases += F;
-	count[i] += GEnv.Freq[x][c]-GEnv.Freq[y][c];
+	//count[i] += GEnv.Freq[x][c]-GEnv.Freq[y][c];
 	   }
-	if(count[i]<0)
+	/*if(count[i]<0)
 	{
 		count[i] = -1 * count[i];
-	}				     
+	}	*/			     
 	Entr /= KnownCases;
     	Entr =(Entr-1)*q;
 					     
-	count[i] /= KnownCases;					     
-	Entr *= count[i];
+	//count[i] /= KnownCases;					     
+	//Entr *= count[i];
 	i++;					     
 	GEnv.MergeEntr[x][y] = Entr;
 }
